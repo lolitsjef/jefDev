13c1961f9394d096b5394193d67539843207c181
lolitsjef <lolitsjef@gmail.com>
2024-10-25 14:30:31 -0600

    rain and sound

--- src/FallingSand.js
+++ src/FallingSand.js
@@ -6,13 +6,19 @@ import rough from 'roughjs/bundled/rough.esm.js';
 
 function App() {
   const chalk = 'rgb(245,245,245)';
-  var grainSize = 10;
+  const maxSounds = 5; 
+  const minTimeBetweenSounds = 50; 
+  let sounds = []; 
+  let lastSoundTime = 0; 
 
-  const w = 800 / grainSize;
-  const h = 400 / grainSize;
+  var grainSize = 8;
+  const w = 720 / grainSize;
+  const h = 1280 / grainSize;
   var depth = 5;
   let isDragging = false;
+  let soundEnabled = false;
   const mousePosition = { x: 0, y: 0, screenX: 0, screenY: 0};
+
 
   var buckets = Array.from({ length: w }, () => Array(h).fill(0));
 
@@ -45,6 +51,8 @@ function App() {
     const rc = rough.canvas(context.canvas);
     rough.newSeed(0);
     depth = parseInt(document.getElementById("depthSlider").value, 10); 
+    soundEnabled = document.getElementById("sound").checked;
+    let played = false;
 
 
     // Update Buckets
@@ -72,6 +80,15 @@ function App() {
             buckets[x][y] = 0;
             continue;
           }
+          else //play sound on setteling
+          {
+            buckets[x][y] = 2;
+            if(!played)
+            {
+              playSound();
+              played = true;
+            }
+          }
         }
       }
     }
@@ -82,14 +99,24 @@ function App() {
       if (mousePosition.x > 0 && mousePosition.x < w-1 && mousePosition.y > 0 && mousePosition.y < h-1) 
       {
         // Assign random 0 or 1 to each bucket position
-        buckets[mousePosition.x][mousePosition.y] = Math.random() > 0.34 ? 1 : 0;
-        buckets[mousePosition.x+1][mousePosition.y] = Math.random() > 0.35 ? 1 : 0;
-        buckets[mousePosition.x][mousePosition.y+1] = Math.random() > 0.35 ? 1 : 0;
-        buckets[mousePosition.x+1][mousePosition.y+1] = Math.random() > 0.35 ? 1 : 0;
-        buckets[mousePosition.x][mousePosition.y] = Math.random() > 0.35 ? 1 : 0;
-        buckets[mousePosition.x-1][mousePosition.y] = Math.random() > 0.35 ? 1 : 0;
-        buckets[mousePosition.x][mousePosition.y-1] = Math.random() > 0.35 ? 1 : 0;
-        buckets[mousePosition.x-1][mousePosition.y-1] = Math.random() > 0.35 ? 1 : 0;
+        buckets[mousePosition.x][mousePosition.y] = Math.random() > 0.34 ? 1 :  buckets[mousePosition.x][mousePosition.y];
+        buckets[mousePosition.x+1][mousePosition.y] = Math.random() > 0.35 ? 1 :  buckets[mousePosition.x][mousePosition.y];
+        buckets[mousePosition.x][mousePosition.y+1] = Math.random() > 0.35 ? 1 :  buckets[mousePosition.x][mousePosition.y];
+        buckets[mousePosition.x+1][mousePosition.y+1] = Math.random() > 0.35 ? 1 :  buckets[mousePosition.x][mousePosition.y];
+        buckets[mousePosition.x][mousePosition.y] = Math.random() > 0.35 ? 1 :  buckets[mousePosition.x][mousePosition.y];
+        buckets[mousePosition.x-1][mousePosition.y] = Math.random() > 0.35 ? 1 :  buckets[mousePosition.x][mousePosition.y];
+        buckets[mousePosition.x][mousePosition.y-1] = Math.random() > 0.35 ? 1 :  buckets[mousePosition.x][mousePosition.y];
+        buckets[mousePosition.x-1][mousePosition.y-1] = Math.random() > 0.35 ? 1 :  buckets[mousePosition.x][mousePosition.y];
+      }
+    }
+
+    //rain
+    if(document.getElementById("rain").checked)
+    {
+      for (let x = 0; x < w; x++)
+      {
+        if(Math.random() > 0.95 && buckets[x][0] === 0)
+        buckets[x][0] = 1;
       }
     }
       
@@ -99,7 +126,7 @@ function App() {
     {
       for (let y = 0; y < h; y++) 
       {
-        if (buckets[x][y] === 1) 
+        if (buckets[x][y] > 0) 
         {
           rc.rectangle(x * grainSize, grainSize * y, grainSize, grainSize, {
             fill: chalk,
@@ -124,6 +151,25 @@ function App() {
       }
       
 
+  };
+
+  const playSound = () => {
+    const currentTime = Date.now(); // Get current time in milliseconds
+    // Check if we can play a new sound
+    if (soundEnabled && sounds.length < maxSounds && (currentTime - lastSoundTime > minTimeBetweenSounds)) {
+      const audio = new Audio(process.env.PUBLIC_URL + '/sounds/sand.mp3');
+      sounds.push(audio); // Add new audio instance to the array
+
+      audio.play().then(() => {
+        lastSoundTime = Date.now(); // Update the last sound time
+      }).catch((error) => {
+        console.log("Error playing sound:", error);
+      });
+
+      audio.onended = () => {
+        sounds = sounds.filter(snd => snd !== audio); // Remove audio instance from the array when it ends
+      };
+    }
   };
 
   const handleMouseDown = (e) => 
@@ -166,8 +212,8 @@ function App() {
             <Canvas 
               draw={draw} 
               sleepDuration = {8}
-              width="800" 
-              height="400"
+              width="720" 
+              height="1280"
               onMouseDown={handleMouseDown}
               onMouseMove={handleMouseMove}
               onMouseUp={handleMouseUp}
@@ -176,8 +222,10 @@ function App() {
 
           {/* Toggles and inputs for project */}
           <section>
-            <div class = "controls">
+            <div className = "controls">
             <wired-button onClick = {reset}>Reset</wired-button>
+            <wired-toggle id = "rain">Rain</wired-toggle>
+            <wired-toggle id = "sound">Sound</wired-toggle>
             <wired-slider id = "depthSlider" value = "2" min = "1" max = "10"> </wired-slider>
             </div>
 
