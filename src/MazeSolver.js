bfdc8a00c72600c955a45c4f2dc613e3cc5b38a9
lolitsjef <lolitsjef@gmail.com>
2024-10-26 15:16:36 -0600

    adding queue

--- src/MazeSolver.js
+++ src/MazeSolver.js
@@ -6,6 +6,8 @@ import rough from 'roughjs/bundled/rough.esm.js';
 
 function App() {
   const chalk = 'rgb(245,245,245)';
+  const green = 'rgb(175, 225, 175)';
+  const red = 'rgb(170, 74, 68)';
 
   var grainSize = 80;
   const w = 800 / grainSize;
@@ -15,15 +17,59 @@ function App() {
   const mousePosition = { x: 0, y: 0, screenX: 0, screenY: 0};
 
   var buckets = Array.from({ length: w }, () => Array(h).fill(0));
-
+  let start = {x: 0, y: 0};
+  let end = {x: w-1, y: h-1};
   const reset = () => 
   {
     buckets = Array.from({ length: w }, () => Array(h).fill(0));
   }
 
-  const draw = async (context, count) => {
+  buckets[start.x][start.y] = 2;
+  buckets[end.x][end.y] = 3;
+
+  class Queue {
+    constructor() {
+      this.items = {};
+      this.frontIndex = 0;
+      this.backIndex = 0;
+    }
+    push(item) {
+      this.items[this.backIndex] = item;
+      this.backIndex++;
+    }
+    pop() {
+      const item = this.items[this.frontIndex];
+      delete this.items[this.frontIndex];
+      this.frontIndex++;
+      return item;
+    }
+  }
+
+  let toVisit = new Queue();
+  toVisit.push(start);
+
+
+
+  
+  const draw = async (context, count) => 
+  {
     context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     const rc = rough.canvas(context.canvas);
+
+
+    // Create Walls
+    if (isDragging) 
+    {
+      if (mousePosition.x >= 0 && mousePosition.x < w && mousePosition.y >= 0 && mousePosition.y < h) 
+      {
+        buckets[mousePosition.x][mousePosition.y] = 1;
+      }
+    }
+
+
+
+
+
 
     // Solve Maze
 
@@ -31,22 +77,65 @@ function App() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+    // Draw to canvas
     // Draw to canvas
     for (let x = 0; x < w; x++) 
-    {
-      for (let y = 0; y < h; y++) 
       {
-          rc.rectangle(x * grainSize, grainSize * y, grainSize, grainSize, {
-            stroke: chalk,
-            roughness: 2,
-            strokeWidth: 0.5
-          });
+        for (let y = 0; y < h; y++) 
+        {
+          if (buckets[x][y] === 1) 
+          {
+            rc.rectangle(x * grainSize, grainSize * y, grainSize, grainSize, {
+              fill: chalk,
+              fillWeight: 2,
+              hachureGap: 3,
+              roughness: 1,
+              stroke: chalk,
+            });
+          }
+          else if (buckets[x][y] === 2) 
+          {
+            rc.rectangle(x * grainSize, grainSize * y, grainSize, grainSize, {
+              fill: green,
+              fillWeight: 2,
+              hachureGap: 3,
+              roughness: 1,
+              stroke: chalk,
+            });
+          }
+          else if (buckets[x][y] === 3) 
+          {
+            rc.rectangle(x * grainSize, grainSize * y, grainSize, grainSize, {
+              fill: red,
+              fillWeight: 2,
+              hachureGap: 3,
+              roughness: 1,
+              stroke: chalk,
+            });
+          }
+          else
+          {
+            rc.rectangle(x * grainSize, grainSize * y, grainSize, grainSize, {
+              roughness: 2,
+              stroke: chalk,
+            });
+          }
+        }
       }
-    }
 
-    if (mousePosition.x >= 0 && mousePosition.x < w && mousePosition.y >= 0 && mousePosition.y < h) 
+    if (mousePosition.screenX > 5 && mousePosition.screenX < 795 && mousePosition.screenY > 5 && mousePosition.screenY < 395) 
       {
-        rc.circle(mousePosition.screenX, mousePosition.screenY, grainSize, {
+        rc.circle(mousePosition.screenX, mousePosition.screenY, grainSize*3/4, {
           fill: chalk,
           stroke: chalk,
           hachureGap: 3,
@@ -117,10 +206,10 @@ function App() {
           {/* Links */}
           <section>
           <wired-card>
-            <wired-link href="https://github.com/lolitsjef/jefDev/blob/main/src/FallingSand.js">Source Code</wired-link>
+            <wired-link href="">Source Code</wired-link>
           </wired-card>
           <wired-card>
-            <wired-link href="https://youtu.be/5MWUf2b7UkY?si=BvHsa6XuVzRRiSDI">YouTube Video</wired-link>
+            <wired-link href="">YouTube Video</wired-link>
           </wired-card>
           </section>
 
